7c33f302a12145740f42d900950e604d810cf29d
PradipSe <47420081+PradipSe@users.noreply.github.com>
2019-06-01 02:25:15 +0530

    Update index.js

--- components/people-detail/index.js
+++ components/people-detail/index.js
@@ -1,5 +1,47 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Slider from "react-slick"
 import './style.less'
+
+class SimpleSlider extends Component {
+    constructor(props) {
+        super(props);
+        this.play = this.play.bind(this);
+        this.pause = this.pause.bind(this);
+    }
+
+    play = () => {
+        this.slider.slickPlay()
+    }
+    
+    pause = () => {
+        this.slider.slickPause()
+    }
+
+    render(){
+        const settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            autoplay: true,
+            slidesToScroll: 1,
+            arrows: false,
+            autoplaySpeed: 2000
+        }
+        return (
+            <div className="sliderWrapper" onMouseEnter={this.play} onMouseLeave={this.pause}>
+                <Slider {...settings} ref={slider => (this.slider = slider)}>
+                    <div>
+                        <img src="https://expertschoice.in/ara/team/group_01.jpg" alt="" />
+                    </div>
+                    <div>
+                        <img src="https://expertschoice.in/ara/team/group_02.jpg" alt="" />
+                    </div>
+                </Slider>
+            </div>
+        );
+    }
+}
 
 function PeopleDetail() {
     return (
@@ -9,15 +51,18 @@ function PeopleDetail() {
                     <div className="column text-content">
                         <div className="left-content">
                             <h2>Our People</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vel tortor purus. Proin ornare tellus a pretium consectetur. Etiam finibus purus ex, a aliquet metus .</p>
+                            <p>ARA Designs characterizes architecture as an art and investigation of ensuring our urban
+                            communities and structures fit with our varied clientele exhibits our diverse work exposure. Our forte is building relationships.
+                            </p>
                         </div>
                     </div>
                     <div className="column column-right">
                         <div className="slider-section">
-                            <img src="https://expertschoice.in/ara/ara-abt-slider.jpg" alt="" />
+                            <SimpleSlider />
                         </div>
                     </div>
                 </div>
+                <div class="container-arrow__arrow"></div>
             </div>
         </div>
     )
